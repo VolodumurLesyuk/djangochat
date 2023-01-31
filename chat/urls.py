6cb833453c84d5db0d0945b43e96f71afd33a71e
VolodumurLesyuk <5647382910z8@gmail.com>
2023-01-31 09:44:35 +0200

    Create django chat app

---
+++ chat/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home, room, checkview, send, getMessages
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('<str:room>/', room, name='room'),
+    path('checkview', checkview, name='checkview'),
+    path('send', send, name='send'),
+    path('getMessages/<str:room>/', getMessages, name='getMessages'),
+]
